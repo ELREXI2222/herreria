a8c91ab30d5139455ea4c3049f0e1b518702f79a
elrexi2222 <reyabel999@gmail.com>
2024-09-24 12:17:51 -0600

    añadimos imagenes a la galeria

--- nustros-trabajos.js
+++ nustros-trabajos.js
@@ -6,6 +6,8 @@ document.getElementById('menu').addEventListener('change', function() {
 
     //endregion
 
+    const vent1 = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg2FMnrFZtppwbIAAaylCSSN6cGl0CTzstxRce1wjl3qdegdxmjY892y7OzYo2CLnK0hnr6wappKD1Idb-s6Wls6XZjhgyQQztV67PHQWpj6rFp1-lThvSa15BVBz9apjQOHtH-y1qrXbv9cdVx6GCuhyphenhyphenop0acUehlFcpLQE1IxPysz1NlfPyFOc_96CwUh/s500/ventana1.webp"
+    const vent2 = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEigrw1OTK4lTEbdy4fC0ltw9sUir3px7Vx1kYRoSA1sZ92iDqPUhhl80ERBUoQbO6OkgnDZJsdNP7b6ln33Zkdl94-bdGjfbIG_5Xx3xYw9wh4VAoT2F0UavaDr3CwqiqeG6-dBPVDO2GNZXRMEVnnbDMOGnP84j8tUTGb0x-JBUC46pV99ulgyPN_AS9_h/s500/ventana2.png"
 
     var selectedValue = this.value;
     var galleryContainer = document.querySelector('.gallery-container .gallery');
@@ -28,8 +30,8 @@ document.getElementById('menu').addEventListener('change', function() {
         `;
     } else if (selectedValue === 'ventana1') {
         galleryContainer.innerHTML = `
-            <img src="src/ventana1_1.jpg" alt="Ventana 1 - Imagen 1">
-            <img src="src/ventana1_2.jpg" alt="Ventana 1 - Imagen 2">
+            <img src="${vent1}" alt="Ventana 1 - Imagen 1">
+            <img src="${vent2}" alt="Ventana 1 - Imagen 2">
         `;
     }else{
         galleryContainer.innerHTML =  `<p>nada que mostrar por aqui</p>`
--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,7 @@
-const verMasButton = document.getElementById('verMas');
-    const modal = document.getElementById('modal');
-    const closeModal = document.querySelector('.modal .close');
 
-    verMasButton.addEventListener('click', function() {
-        modal.classList.remove('hidden');
-    });
 
-    closeModal.addEventListener('click', function() {
-        modal.classList.add('hidden');
-    });
+
+
 
     window.addEventListener('click', function(event) {
         if (event.target === modal) {
@@ -21,6 +14,9 @@ const verMasButton = document.getElementById('verMas');
         fetch('header.html')
             .then(response => response.text())
             .then(data => document.getElementsByClassName('header').innerHTML = data);
+
+
+
 
 
            
